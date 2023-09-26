9b89fea16b2568905781ebbd1399b113f9321f5d
Marcia Klimisch <93682145+Khaleesi89@users.noreply.github.com>
2023-09-25 21:09:06 -0300

    borrado de archivos no utilizables

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthProvider } from './components/context/AuthProvider'
 import axios from 'axios'
 
 
@@ -10,8 +9,8 @@ axios.defaults.baseURL = 'http://localhost:8000/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
+    
       <App />
-    </AuthProvider>
+   
   </React.StrictMode>,
 )
